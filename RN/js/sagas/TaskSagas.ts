ce6719c0dcc7f45d9bd72f8c4f9d4b167c9121c7
Dan <danpanuska@gmail.com>
2020-12-29 21:58:17 -0500

    Remove unused

--- RN/js/sagas/TaskSagas.ts
+++ RN/js/sagas/TaskSagas.ts
@@ -1,28 +1,28 @@
-import {call, put, takeLatest} from 'redux-saga/effects';
-import * as actions from '../constants/Actions';
+// import {call, put, takeLatest} from 'redux-saga/effects';
+// import * as actions from '../constants/Actions';
 
-function* executeTask(action) {
-    try {
-        let {task} = action.payload;
-        var subTask;
-        for (subTask in task.taskList) {
-            if (subTask.suspend === true) {
-                yield call(executeSubTask, subTask);
-            } else {
-                call(executeSubTask, subTask);
-            }
-        }
-        yield put({type: actions.DISPATCH_TASK_SUCCEEDED});
-    } catch (error) {
-        yield put({type: actions.DISPATCH_TASK_FAILED, error});
-    }
-}
-// TODO should we expect taskList always, or be able to execute
-function* executeSubTask(task) {
-    try {
-    } catch (error) {}
-}
+// function* executeTask(action) {
+//     try {
+//         let {task} = action.payload;
+//         var subTask;
+//         for (subTask in task.taskList) {
+//             if (subTask.suspend === true) {
+//                 yield call(executeSubTask, subTask);
+//             } else {
+//                 call(executeSubTask, subTask);
+//             }
+//         }
+//         yield put({type: actions.DISPATCH_TASK_SUCCEEDED});
+//     } catch (error) {
+//         yield put({type: actions.DISPATCH_TASK_FAILED, error});
+//     }
+// }
+// // TODO should we expect taskList always, or be able to execute
+// function* executeSubTask(task) {
+//     try {
+//     } catch (error) {}
+// }
 
-export default function* rootSaga() {
-    yield takeLatest(actions.DISPATCH_TASK_REQUESTED, executeTask);
-}
+// export default function* rootSaga() {
+//     yield takeLatest(actions.DISPATCH_TASK_REQUESTED, executeTask);
+// }
