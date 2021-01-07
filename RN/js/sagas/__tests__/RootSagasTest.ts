2e127cdfa58acf6782c3a91fb02d7a04d31ca8d3
Dan <>
2021-01-07 17:23:33 -0500

    Fix test imports

--- RN/js/sagas/__tests__/RootSagasTest.ts
+++ RN/js/sagas/__tests__/RootSagasTest.ts
@@ -1,19 +1,5 @@
-import {expectSaga, testSaga} from 'redux-saga-test-plan';
-import rootSaga from '../RootSagas';
-import appSagas from '../AppSagas';
-import ttsSagas from '../TTSSagas';
-import cameraSagas from '../CameraSagas';
-import taskSagas from '../TaskSagas';
-import {APP_INITIALIZE_SERVICES} from '../../constants/Actions';
+import rootSagas from '../RootSagas';
 
 describe('RootSagas', () => {
     it('is empty right now', () => {});
-    // it('should spawn all root sagas and dispatch initialize action', () => {
-    //     expectSaga(rootSaga)
-    //         .spawn(appSagas)
-    //         .spawn(ttsSagas)
-    //         .spawn(cameraSagas)
-    //         .spawn(taskSagas)
-    //         .put({type: APP_INITIALIZE_SERVICES});
-    // });
 });
